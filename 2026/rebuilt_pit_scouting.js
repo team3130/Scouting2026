838772dbba06a1c8c9b214923984605eae699110
Umar K <umarkhanayatampa@gmail.com>
2026-03-02 17:15:25 -0600

    adding kraken to pit scouting list

--- 2026/rebuilt_pit_scouting.js
+++ 2026/rebuilt_pit_scouting.js
@@ -60,6 +60,7 @@ var config_data = `
         "n": "Neo<br>",
         "f": "Falcon<br>",
         "c": "CIM<br>",
+        "k": "Kraken", 
         "x": "Other<br>"
       },
       "defaultValue":"x"
